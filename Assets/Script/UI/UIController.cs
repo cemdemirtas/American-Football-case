585b36cd1fed785efe5d93e7ef82ea61550e066c
Cem Demirtas <97852548+cemdemirtas@users.noreply.github.com>
2022-07-05 19:38:53 +0300

    Progressbar

---
+++ Assets/Script/UI/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    float distance, restOfDistance;
+    [SerializeField] Image Progressbar;
+    [SerializeField] Transform finishPoint;
+    Vector3 beginPoint;
+
+    private void Awake()
+    {
+        beginPoint = new Vector3(0, 0, -84.406f);
+
+    }
+    private void Update()
+    {
+
+        distance = Vector3.Distance(finishPoint.position, beginPoint);
+        restOfDistance = Vector3.Distance(PlayerMovement.instance.transform.position, beginPoint);
+        Progressbar.fillAmount = 1 - (restOfDistance / distance)  ;
+    }
+}
